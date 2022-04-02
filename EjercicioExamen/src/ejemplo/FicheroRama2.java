55457615a02773fdc61f4734a81e7025e1571109
sergi <sergi@DESKTOP-95SO370>
2022-04-02 18:00:17 +0200

    Cambio para contar consonantes en FicheroRama2

--- EjercicioExamen/src/ejemplo/FicheroRama2.java
+++ EjercicioExamen/src/ejemplo/FicheroRama2.java
@@ -2,7 +2,7 @@ package ejemplo;
 
 import java.util.Scanner;
 
-public class String1 {
+public class FicheroRama2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -12,6 +12,7 @@ public class String1 {
 		System.out.println("Introduce la frase que quieres analizar");
 		String frase = sc.nextLine();
 		int numVocales = 0;
+		int numConsonantes=0;
 
 		for (int i = 0; i < frase.length(); i++) {
 			if ((frase.charAt(i) == 'a') || (frase.charAt(i) == 'e') || (frase.charAt(i) == 'i')
@@ -19,8 +20,16 @@ public class String1 {
 				numVocales++;
 			}
 		}
+		
+		for (int i = 0; i < frase.length(); i++) {
+			if ((frase.charAt(i) != 'a') || (frase.charAt(i) != 'e') || (frase.charAt(i) != 'i')
+					|| (frase.charAt(i) != 'o') || (frase.charAt(i) != 'u')) {
+				numConsonantes++;
+			}
+		}
 
 		System.out.println("El numero de vocales que tiene la frase es:" + numVocales);
+		System.out.println("El numero de consonantes que tiene la frase es:" + numConsonantes);
 		sc.close();
 
 	}
